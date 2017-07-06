d26253507d7dbb1967a96bb25283a1638aeeceb0
rsakie <rsakie@yahoo.com>
2017-07-06 10:10:54 -0400

    practice with for loops

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -10,6 +10,41 @@ namespace LoopsPractice
     {
         static void Main(string[] args)
         {
+            //when would you use a loop
+            //why would you use a loop
+            //nested loops
+            //infinite loops
+            //keywords break and continue
+
+            //for loop
+            //can be used for counting and or...
+            // ITERATING through an array (or other collection typs)
+            //looks like this
+            //for(initalizer; condition; updater)
+            //{
+            //   do something
+            //}
+
+            //for(int i = 15; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for(int i= 50; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //string[] days = { "sun", "mon", "tues", "wed", "thurs", "fri", "sat" };
+
+            //for (int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+
+           
+
+
+
         }
     }
 }
